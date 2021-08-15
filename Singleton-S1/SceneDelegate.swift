af249129524662ba32ecf169ef1ed4d776e1207f
Ashish Rai <ash09rai@gmail.com>
2021-08-15 22:50:43 +0530

    Description: Accessing the loginUser using composer and directly

--- Singleton-S1/SceneDelegate.swift
+++ Singleton-S1/SceneDelegate.swift
@@ -17,12 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-//        let loginVM = LoginComposer.init(service: LoginAdapter())
-//        loginVM.composeLoginModule()
+        //Using composer
+        let loginVM = LoginComposer.init(service: LoginAdapter())
+        loginVM.composeLoginModule()
         
         /*
          Adapter functionality
          */
+        //Without Composer
         var loginVC = Login2ViewModel()
         let user = LoggedInuser(name: "randy")
         loginVC.loginUser = { completion in
